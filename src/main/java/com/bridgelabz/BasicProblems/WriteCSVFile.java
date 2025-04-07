a3a2e7120aa782a5c235c003de8d498c76a98df0
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-04-07 15:30:27 +0530

    Program to write string to a csv file using buffer writer

---
+++ src/main/java/com/bridgelabz/BasicProblems/WriteCSVFile.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.BasicProblems;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteCSVFile {
+    public static void main(String[] args) {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\dell\\Desktop\\bridgelabz-workspace\\java-csv-data-handling\\src\\main\\java\\com\\bridgelabz\\BasicProblems\\studentData.csv",true))){
+            bw.newLine();
+            String[] arr = {"1,Ishaan,cse,10","2,Hitesh,cse,5"};
+            for(String i:arr){
+                bw.write(i + "\n");
+            }
+//            bw.write();
+//            bw.write(arr[1] + "\n");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
